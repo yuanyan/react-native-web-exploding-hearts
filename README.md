0e6a821a659f0875be0b0ef968b9a5008e83d081
元彦 <yuanyan.cao@alibaba-inc.com>
2016-01-12 11:47:05 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# React Native Web Exploding Hearts
+
+![screenshot](http://img1.tbcdn.cn/L1/461/1/525fb1fc871490d02e600659a10a5cc2bde3b221.png)
+
+## Usage
+Install dependencies and devDependencies:
+```
+npm install
+```
+
+Start native packager server:
+```
+npm start
+```
+
+Start web dev server:
+```
+npm run web-dev
+```
+
+Build web bundle:
+```
+npm run web-bundle
+```
---
+++ web/webpack.config.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlPlugin = require('webpack-html-plugin');
+var HasteResolverPlugin = require('haste-resolver-webpack-plugin');
+
+var IP = '0.0.0.0';
+var PORT = 3000;
+var NODE_ENV = process.env.NODE_ENV;
+var ROOT_PATH = path.resolve(__dirname, '..');
+
+let isProd = NODE_ENV === 'production';
+
+var config = {
+  paths: {
+    src: path.join(ROOT_PATH, '.'),
+    index: path.join(ROOT_PATH, 'index.ios'),
+  },
+};
+
+module.exports = {
+  ip: IP,
+  port: PORT,
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      'react-native': 'react-web',
+      'ReactNativeART': 'react-art',
+    },
+    extensions: ['', '.js', '.jsx'],
+  },
+  entry: isProd? [
+    config.paths.index
+  ]: [
+    'webpack-dev-server/client?http://' + IP + ':' + PORT,
+    'webpack/hot/only-dev-server',
+    config.paths.index,
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HasteResolverPlugin({
+      platform: 'web',
+      nodeModules: ['react-web']
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.json$/,
+      loader: 'json',
+    }, {
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel?stage=1'],
+      include: [config.paths.src],
+      exclude: [/node_modules/]
+    }, ]
+  }
+};
